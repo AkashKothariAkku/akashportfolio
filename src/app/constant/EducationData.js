84ea60f407d1bbe63c84b9be10b1bf05672f3899
Akash <akashkothari80@gmail.com>
2024-03-17 21:04:19 +0530

    updated

---
+++ src/app/constant/EducationData.js
@@ -0,0 +1,27 @@
+export const experienceData = [{
+            year: "May,2022 - Oct-2023",
+            post: "MERN Stack Developer",
+            company: "Appic Softwares"
+},{
+    year: "Jan,2021 - Apr-2022",
+    post: "Frontend Developer",
+    company: "Adsfuturistic"
+},{
+    year: "July,2020 - Dec-2020",
+    post: "Wordpress Developer",
+    company: "Yessoftech"
+}]
+
+export const educationData = [{
+    year: "2017-2020",
+    course: "BCA",
+    university: "Maharaja University"
+},{
+year: "2015-2016",
+course: "Class XII",
+university: "Dhruva Bal Niketan Senior Secondary School"
+},{
+    year: "2013-2014",
+    course: "Class X",
+    university: "Dhruva Bal Niketan Senior Secondary School"
+}]
